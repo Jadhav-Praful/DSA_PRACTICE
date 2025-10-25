6bebfab6cd6b57a234198b5ad915732e14cd1d0e
Jadhav-Praful-Nayak <jadhavprafulnayak2000@gmail.com>
2025-10-25 13:18:53 +0530

    prob4 completed

--- DP/prob4.java
+++ DP/prob4.java
@@ -1,5 +1,6 @@
 package DP;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*Problem Statement:
@@ -17,7 +18,14 @@ public class prob4 {
         int notPick = brute(ind-1, arr);
         return Math.max(pick, notPick);
     }
-
+    static int optimal(int n,int arr[],int dp[]){
+        dp[0]=arr[0];
+        dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],arr[i]+(i>=2?dp[i-2]:0));
+        }
+        return dp[n-1];
+    }
     
     public static void main(String[] args) {
         int n;
@@ -29,7 +37,11 @@ public class prob4 {
             arr[i]= sc.nextInt();
         }
         int answer = brute(n-1, arr);
-        System.out.println("Maximum sum of subsequence is:"+answer);
+        System.out.println("Maximum sum of subsequence is brute:"+answer);
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        int ans=optimal(n,arr,dp);
+        System.out.println("Maximum sum of subsequence is optimal:"+ans);
         sc.close();
     }
 }
